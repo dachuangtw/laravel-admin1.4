d22cce423009ec0dbde92f0e8740a39e86923d8d
淑貞 <34149510+shujhen@users.noreply.github.com>
2017-12-08 16:45:57 +0800

    倉庫

---
+++ database/migrations/2017_12_08_163951_create_warehouse_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWarehouseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warehouse', function (Blueprint $table) {
+            $table->increments('wid')->comment('倉庫id');
+            $table->string('w_name',25)->comment('倉庫名');
+            $table->string('w_phone',50)->comment('連絡電話');
+            $table->string('w_postcode',25)->comment('郵遞區號');
+            $table->string('w_city',25)->comment('縣市');
+            $table->string('w_area',25)->comment('鄉鎮市區');
+            $table->string('w_street',100)->comment('街道地址');
+            $table->text('w_notes')->comment('備註');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warehouse');
+    }
+}

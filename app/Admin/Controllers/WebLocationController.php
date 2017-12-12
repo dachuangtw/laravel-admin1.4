7f76e5b161fecf33e38dd68d13466dbd58e98b5c
WangYiNing <we820731@gmail.com>
2017-12-12 15:29:56 +0800

    店鋪據點(地區未完成)

--- app/Admin/Controllers/WebLocationController.php
+++ app/Admin/Controllers/WebLocationController.php
@@ -74,10 +74,15 @@ class WebLocationController extends Controller
         return Admin::grid(WebLocation::class, function (Grid $grid) {
 
             $grid->id(trans('admin::lang.store_id'))->sortable();
-            $grid->store_area(trans('admin::lang.store_area'));
+            $grid->store_area(trans('admin::lang.store_area'))->sortable();
             $grid->store_name(trans('admin::lang.store_name'));
             $grid->created_at(trans('admin::lang.created_at'));
             $grid->updated_at(trans('admin::lang.updated_at'));
+            $states = [
+                'on'  => ['value' => 1, 'text' => 'ON', 'color' => 'success'],
+                'off' => ['value' => 2, 'text' => 'OFF', 'color' => 'danger'],
+            ];
+            $grid->column('showfront',trans('admin::lang.showfront'))->status()->switch($states);
         });
     }
 
@@ -91,14 +96,20 @@ class WebLocationController extends Controller
         return Admin::form(WebLocation::class, function (Form $form) {
 
             $form->display('id', trans('admin::lang.store_id'));
-           $form->text('store_name', trans('admin::lang.store_name'));
-            $form->select('store_area',trans('admin::lang.store_area'))->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
+            $form->text('store_name', trans('admin::lang.store_name'));
+            $form->select('store_area', trans('admin::lang.store_area'))->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
             $form->text('store_address', trans('admin::lang.store_address'));
-            $form->text('GPS', 'GPS');
+            $form->editor('map',trans('admin::lang.store_map'));
+            $form->html('<a href="https://goo.gl/13yFtr">嵌入地圖說明</a>');
+
+            //$form->map($latitude, $longitude,'GPS'); //經度,緯度
+            $form->image('store_pic', trans('admin::lang.store_pic'))->move('/location','store_pic');
+            $states = [
+                'on'  => ['value' => 1, 'text' => 'ON', 'color' => 'success'],
+                'off' => ['value' => 0, 'text' => 'OFF', 'color' => 'danger'],
+            ];
+            $form->switch('showfront',trans('admin::lang.showfront'))->states($states);
             $form->textarea('comment',trans('admin::lang.comment'))->rows(10);
-
-
-
             $form->display('created_at',trans('admin::lang.created_at'));
             $form->display('updated_at',trans('admin::lang.updated_at'));
         });

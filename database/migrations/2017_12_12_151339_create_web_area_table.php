f03abc24d41292d50135798947dd20c1b8bca80c
WangYiNing <we820731@gmail.com>
2017-12-12 15:22:46 +0800

    新增地區

---
+++ database/migrations/2017_12_12_151339_create_web_area_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWebAreaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('web_area', function (Blueprint $table) {
+            $table->increments('id')->unique()->index()->comment('地區id');
+            $table->string('area')->comment('地區');
+            $table->string('area_name')->comment('名稱');
+            $table->integer('area_sort')->default(0)->comment('順序');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('web_area');
+    }
+}

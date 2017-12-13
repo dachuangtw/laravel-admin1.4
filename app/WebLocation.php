66cd823d287a8cc9807761b7573f9cd40dfe4a27
WangYiNing <we820731@gmail.com>
2017-12-13 16:55:18 +0800

    更新店鋪據點

--- app/WebLocation.php
+++ app/WebLocation.php
@@ -10,26 +10,4 @@ class WebLocation extends Model
     protected $table = 'web_location';
     //主鍵
     protected $primaryKey = 'id';
-    
-    /*public function tags()
-    {
-        return $this->belongsToMany(Tag::class);
-    }*/
-
-    public function getWebLocationAttribute($store_name)
-    {
-        if (is_string($store_name)) {
-            return json_decode($store_name, true);
-        }
-
-        return $store_name;
-    }
-
-    public function setWebLocationAttribute($store_name)
-    {
-        if (is_array($store_name)) {
-            $this->attributes['store_name'] = json_encode($store_name);
-        }
-    }
-
 }

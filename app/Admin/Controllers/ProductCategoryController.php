784a4bdccde554007881fd980b1019f4759f2ed0
淑貞 <34149510+shujhen@users.noreply.github.com>
2017-12-13 08:58:35 +0800

    頁面排版改成與menu頁一樣，在同一頁可做新增跟排序

--- app/Admin/Controllers/ProductCategoryController.php
+++ app/Admin/Controllers/ProductCategoryController.php
@@ -7,10 +7,13 @@ use App\ProductCategory;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
 use Encore\Admin\Tree;
+use Encore\Admin\Widgets\Box;
 
 class ProductCategoryController extends Controller
 {
@@ -28,8 +31,35 @@ class ProductCategoryController extends Controller
             $content->header(trans('admin::lang.product_category'));
             $content->description(trans('admin::lang.list'));
             
-            $content->body(ProductCategory::tree());
-            // $content->body($this->grid());
+            //$content->body(ProductCategory::tree());
+
+            $content->row(function (Row $row) {
+                $row->column(6, $this->treeView()->render());
+
+                $row->column(6, function (Column $column) {
+                    $form = new \Encore\Admin\Widgets\Form();
+                    $form->action(admin_url('product/category'));
+
+                    $form->text('pc_name', trans('admin::lang.name'))->rules('required');
+
+                    $column->append((new Box(trans('admin::lang.new'), $form))->style('danger'));
+                });
+            });
+        });
+    }
+
+    /**
+     * @return \Encore\Admin\Tree
+     */
+    protected function treeView()
+    {
+        return ProductCategory::tree(function (Tree $tree) {
+            $tree->disableCreate();
+
+            $tree->branch(function ($branch) {
+                $payload = "&nbsp;<strong>{$branch['pc_name']}</strong>";
+                return $payload;
+            });
         });
     }
 

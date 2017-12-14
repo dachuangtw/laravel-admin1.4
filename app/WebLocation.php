de9976557ab553563677cf2332e0b34d6b09ee39
WangYiNing <we820731@gmail.com>
2017-12-14 16:54:13 +0800

    更改店鋪據點及地區tree方式(未完成)

--- app/WebLocation.php
+++ app/WebLocation.php
@@ -3,11 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Encore\Admin\Traits\ModelTree;
+use Encore\Admin\Traits\AdminBuilder;
 
 class WebLocation extends Model
 {
+	use ModelTree, AdminBuilder;
+
     //店鋪據點資料表
     protected $table = 'web_location';
     //主鍵
     protected $primaryKey = 'id';
+
+	// tree srot
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->setParentColumn('store_area'); //parent_id
+        $this->setOrderColumn('store_sort'); //order
+        $this->setTitleColumn('store_name'); //title
+    }
+
+    public function web_area()
+    {
+        return $this->belongsTo(WebArea::class);
+    }
 }

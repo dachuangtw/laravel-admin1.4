1a8ba96239c24ca8b8b0fd6491f93553c46ad2fc
淑貞 <34149510+shujhen@users.noreply.github.com>
2017-12-14 08:55:27 +0800

    修改欄位

--- database/migrations/2017_12_08_165145_create_product_index_table.php
+++ database/migrations/2017_12_08_165145_create_product_index_table.php
@@ -30,7 +30,7 @@ class CreateProductIndexTable extends Migration
             $table->text('p_series')->nullable()->comment('主題系列勾選(用|分隔)');
             $table->text('p_notes')->nullable()->comment('備註');
             $table->boolean('showfront')->default(true)->comment('前台顯示');
-            $table->boolean('showsales')->default(true)->comment('業務顯示');
+            $table->boolean('shownew')->default(true)->comment('新荷入庫顯示');
             $table->string('update_user',25)->nullable()->comment('最後更新者');
             $table->timestamps();
             $table->softDeletes();              

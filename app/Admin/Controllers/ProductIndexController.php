50c6034f73c42b0dee8d324fe5261c62838fe9e2
淑貞 <34149510+shujhen@users.noreply.github.com>
2017-12-14 17:07:20 +0800

    不停的修改

--- app/Admin/Controllers/ProductIndexController.php
+++ app/Admin/Controllers/ProductIndexController.php
@@ -10,9 +10,12 @@ use App\ProductCategory;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Widgets\Box;
 
 class ProductIndexController extends Controller
 {
@@ -30,7 +33,22 @@ class ProductIndexController extends Controller
             $content->header(trans('admin::lang.product_index'));
             $content->description(trans('admin::lang.list'));
 
-            $content->body($this->grid());
+            $content->row(function (Row $row) {
+                $row->column(6, function (Column $column) {
+                    $form = new \Encore\Admin\Widgets\Form();
+                    $form->action(admin_url('product'));
+
+                    $form->text('p_name', trans('admin::lang.name'))->rules('required');
+
+                    $column->append((new Box(trans('admin::lang.new'), $form))->style('info'));
+                });
+
+            });
+            $content->row(function (Row $row) {
+                $row->column(12, $this->grid());
+
+            });
+            // $content->body($this->grid());
         });
     }
 
@@ -63,6 +81,8 @@ class ProductIndexController extends Controller
             $content->header(trans('admin::lang.product_index'));
             $content->description(trans('admin::lang.create'));
 
+            
+
             $content->body($this->form());
         });
     }
@@ -76,17 +96,37 @@ class ProductIndexController extends Controller
     {
         return Admin::grid(ProductIndex::class, function (Grid $grid) {
 
+            $grid->filter(function ($filter) {
+                $filter->like('p_name','測試');
+            });
+
+            $grid->rows(function($row){
+                if($row->id % 2) {
+                    $row->style('background-color:red');
+                }
+            });
+
             $grid->pid('ID')->sortable();
             $grid->p_number(trans('admin::lang.product_number'))->sortable();
             $grid->p_name(trans('admin::lang.name'));
             $grid->p_pic(trans('admin::lang.product_pic'))->display(function ($p_pic) {                
-                return "<img src='".rtrim(config('admin.upload.host'), '/').'/'.$p_pic."' style='max-width: 200px;max-height: 200px;'/>";            
+                return "<img src='".rtrim(config('admin.upload.host'), '/').'/'.$p_pic."' style='max-width:50px;max-height:50px;' onerror='this.src=\"".config('app.url')."/images/404.jpg\"'/>";            
             });
             $grid->p_salesprice(trans('admin::lang.product_salesprice'));
             $grid->p_costprice(trans('admin::lang.product_costprice'));
-
+            $grid->showfront('前台顯示')->value(function ($showfront) {
+                return $showfront ? "<span class='label label-success'>Yes</span>" : "<span class='label label-danger'>No</span>";
+            });
+            $grid->showsales('業務顯示')->value(function ($showsales) {
+                return $showsales ? "<span class='label label-success'>Yes</span>" : "<span class='label label-danger'>No</span>";
+            });
             $grid->updated_at(trans('admin::lang.updated_at'));
             $grid->created_at(trans('admin::lang.created_at'));
+            $grid->filter(function ($filter) {
+                
+                // 设置created_at字段的范围查询
+                $filter->between('created_at', 'Created Time')->datetime();
+            });
         });
     }
 
@@ -109,7 +149,7 @@ class ProductIndexController extends Controller
             $form->currency('p_retailprice', trans('admin::lang.product_retailprice'))->options(['digits' => 0]);
             $form->currency('p_specialprice', trans('admin::lang.product_specialprice'))->options(['digits' => 0]);
             $form->currency('p_salesprice', trans('admin::lang.product_salesprice'))->options(['digits' => 0]);
-            $form->currency('p_staffprice', trans('admin::lang.product_staffprice'))->options(['digits' => 0]);
+            // $form->currency('p_staffprice', trans('admin::lang.product_staffprice'))->options(['digits' => 0]);
             $form->currency('p_costprice', trans('admin::lang.product_costprice'))->options(['digits' => 0]);
 
             $form->multipleSelect('p_category', trans('admin::lang.product_category'))->options(
@@ -125,6 +165,7 @@ class ProductIndexController extends Controller
             
             $form->switch('showfront', trans('admin::lang.showfront'))->states($states)->default(1);
             $form->switch('shownew', trans('admin::lang.shownew'))->states($states)->default(1);
+            $form->switch('showsales', trans('admin::lang.showsales'))->states($states)->default(1);
 
             $form->textarea('p_notes', trans('admin::lang.notes'))->rows(5);
 

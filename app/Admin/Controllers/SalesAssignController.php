48d793d308506a14bc2addf7e9d4fdd945cd89ce
WangYiNing <we820731@gmail.com>
2017-12-15 17:03:39 +0800

    每日配貨(未完成)

--- app/Admin/Controllers/SalesAssignController.php
+++ app/Admin/Controllers/SalesAssignController.php
@@ -7,9 +7,12 @@ use App\SalesAssign;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Widgets\Box;
 
 class SalesAssignController extends Controller
 {
@@ -24,8 +27,8 @@ class SalesAssignController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header(trans('admin::lang.sales_assign'));
+            $content->description(trans('admin::lang.list'));
 
             $content->body($this->grid());
         });
@@ -41,8 +44,8 @@ class SalesAssignController extends Controller
     {
         return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header(trans('admin::lang.sales_assign'));
+            $content->description(trans('admin::lang.edit'));
 
             $content->body($this->form()->edit($id));
         });
@@ -57,10 +60,21 @@ class SalesAssignController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header(trans('admin::lang.sales_assign'));
+            $content->description(trans('admin::lang.create'));
 
             $content->body($this->form());
+            $content->row(function (Row $row) {
+                $row->column(6, function (Column $column) {
+                    $form = new \Encore\Admin\Widgets\Form();
+                    $form->action(admin_url('product'));
+
+                    $form->text('p_name', trans('admin::lang.name'))->rules('required');
+
+                    $column->append((new Box(trans('admin::lang.new'), $form))->style('info'));
+                });
+
+            });
         });
     }
 
@@ -74,9 +88,12 @@ class SalesAssignController extends Controller
         return Admin::grid(SalesAssign::class, function (Grid $grid) {
 
             $grid->id('ID')->sortable();
+            $grid->assign_date(trans('admin::lang.assign_date'))->sortable();
+            $grid->assign_id(trans('admin::lang.assign_id'))->sortable();
+            $grid->assign_total(trans('admin::lang.assign_total'));
 
-            $grid->created_at();
-            $grid->updated_at();
+            $grid->created_at(trans('admin::lang.created_at'));
+            $grid->updated_at(trans('admin::lang.updated_at'));
         });
     }
 
@@ -90,9 +107,10 @@ class SalesAssignController extends Controller
         return Admin::form(SalesAssign::class, function (Form $form) {
 
             $form->display('id', 'ID');
+            $form->datetime('assign_date',trans('admin::lang.assign_date'))->format('YYYY-MM-DD HH:mm:ss');
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->display('created_at',trans('admin::lang.created_at'));
+            $form->display('updated_at',trans('admin::lang.updated_at'));
         });
     }
 }

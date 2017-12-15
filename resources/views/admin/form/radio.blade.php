29ccbb29cf912e9ec37fb723fe7216fcc6bc9df2
淑貞 <34149510+shujhen@users.noreply.github.com>
2017-12-15 15:53:46 +0800

    修改

--- resources/views/admin/form/radio.blade.php
+++ resources/views/admin/form/radio.blade.php
@@ -9,7 +9,7 @@
         @foreach($options as $option => $label)
             <div class="radio">
                 <label>
-                    <input type="radio" name="{{$name}}" value="{{$option}}" class="minimal {{$class}}" {{ ($option == old($column, $value))?'checked':'' }} />&nbsp;{{$label}}&nbsp;&nbsp;
+                    <input type="radio" name="{{$name}}" value="{{$option}}" class="minimal {{$class}}" {{ ($option == old($column, $value))?'checked':'' }} />{{$label}}&nbsp;&nbsp;&nbsp;
                 </label>
             </div>
         @endforeach

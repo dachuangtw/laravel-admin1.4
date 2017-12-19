603d51f8090370d61d30d5eb87d4dba778a71c01
WangYiNing <we820731@gmail.com>
2017-12-19 09:07:17 +0800

    業務新增到職日&離職日

---
+++ database/migrations/2017_12_19_083718_add_work_date_to_sales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWorkDateToSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->string('start_work_date')->nullable()->after('resign');
+            $table->string('end_work_date')->nullable()->after('start_work_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropColumn('start_work_date');
+            $table->dropColumn('end_work_date');
+        });
+    }
+}

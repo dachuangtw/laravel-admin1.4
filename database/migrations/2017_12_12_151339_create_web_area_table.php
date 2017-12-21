700df752829d1856752d26fb7463232638bbda3d
WangYiNing <we820731@gmail.com>
2017-12-21 15:38:23 +0800

    新增type

--- database/migrations/2017_12_12_151339_create_web_area_table.php
+++ database/migrations/2017_12_12_151339_create_web_area_table.php
@@ -17,7 +17,7 @@ class CreateWebAreaTable extends Migration
             $table->increments('id')->unique()->index()->comment('地區id');
             $table->integer('parent_id')->default(0)->comment('父級ID');
             $table->integer('area_sort')->default(0)->comment('順序');
-            //$table->tinyInteger('type')->default(2);
+            $table->tinyInteger('type')->default(2);
             $table->string('area_name')->comment('地區名稱');
             $table->string('zipcode')->nullable()->comment('郵遞區號');
             //$table->timestamps();

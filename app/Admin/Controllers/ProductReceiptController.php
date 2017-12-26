0866455ae107ef70d8d03e361a51c7cae650e5ce
淑貞 <34149510+shujhen@users.noreply.github.com>
2017-12-26 17:05:48 +0800

    開始做廠商進貨單功能

---
+++ app/Admin/Controllers/ProductReceiptController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\ProductReceipt;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class ProductReceiptController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        Permission::check(['reader']);
+        return Admin::content(function (Content $content) {
+
+            $content->header(trans('admin::lang.product_receipt'));
+            $content->description(trans('admin::lang.list'));
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header(trans('admin::lang.product_receipt'));
+            $content->description(trans('admin::lang.edit'));
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header(trans('admin::lang.product_receipt'));
+            $content->description(trans('admin::lang.create'));
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(ProductReceipt::class, function (Grid $grid) {
+
+            $grid->id('ID')->sortable();
+
+            $grid->created_at();
+            $grid->updated_at();
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(ProductReceipt::class, function (Form $form) {
+
+            $form->display('id', 'ID');
+
+            $form->display('created_at', 'Created At');
+            $form->display('updated_at', 'Updated At');
+        });
+    }
+}
---
+++ database/migrations/2017_12_26_165807_create_product_receipt_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductReceiptTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_receipt', function (Blueprint $table) {            
+            $table->increments('reid')->comment('庫存id');
+            $table->integer('supid')->unsigned()->index()->comment('廠商id');
+            $table->integer('wid')->unsigned()->index()->comment('進貨倉庫id');
+            $table->integer('re_user')->unsigned()->index()->comment('進貨人員id');
+            $table->decimal('re_amount',10,2)->default(0.00)->comment('總金額'); 
+            $table->text('re_notes')->nullable()->comment('備註');
+            $table->timestamp('re_delivery')->nullable()->comment('進貨日');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_receipt');
+    }
+}

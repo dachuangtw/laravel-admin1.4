c3acb02fb32fc5be5052baf8f68c0e6edccc091c
淑貞 <34149510+shujhen@users.noreply.github.com>
2017-12-28 15:09:58 +0800

    增加另一個預設選取的情況

--- resources/views/admin/form/select.blade.php
+++ resources/views/admin/form/select.blade.php
@@ -10,7 +10,7 @@
 
         <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}" {!! $attributes !!} >
             @foreach($options as $select => $option)
-                <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}>{{$option}}</option>
+                <option value="{{$select}}" {{ ($select == old($column, $value))||((string)$select === request($name, $value)) ?'selected':'' }}>{{$option}}</option>
             @endforeach
         </select>
 

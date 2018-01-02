90ab59d36a979edd88066f04f5fec8bd88017aa7
淑貞 <shujhen@outlook.com>
2018-01-02 08:41:24 +0800

    Database:進貨單明細

---
+++ database/migrations/2018_01_02_082321_create_product_receipt_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductReceiptDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_receipt_details', function (Blueprint $table) {
+            $table->increments('redid')->comment('進貨單明細id');
+            $table->string('re_number',50)->comment('進貨單號');
+            $table->integer('pid')->unsigned()->index()->comment('商品主檔id');
+            $table->integer('red_quantity')->default(0)->comment('數量');
+            $table->decimal('red_price',10,2)->default(0.00)->comment('單價'); 
+            $table->decimal('red_amount',10,2)->default(0.00)->comment('總金額'); 
+            $table->string('red_notes',100)->nullable()->comment('備註');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_receipt_details');
+    }
+}

6ef7d0ef18fe59f10f36611a74529494a04d558a
淑貞 <shujhen@outlook.com>
2018-01-03 14:50:20 +0800

    div欄位resize功能(未完成)

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -23,3 +23,4 @@ app('view')->prependNamespace('admin', resource_path('views/admin'));
 Admin::css('css/customize.css');
 Admin::js('js/customize.js');
 Admin::js('js/popper.min.js');
+Admin::js('js/jquery-resizable.js');

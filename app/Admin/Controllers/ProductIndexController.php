1d2abec7f50f4c67b7882c33db839056f8a39f58
shujhen <shujhen@outlook.com>
2018-01-11 13:57:41 +0800

    前次Git小修改

--- app/Admin/Controllers/ProductIndexController.php
+++ app/Admin/Controllers/ProductIndexController.php
@@ -577,24 +577,6 @@ class ProductIndexController extends Controller
 
             if(!empty($data) && $data->count())
             {
-                <<<EOT
-                <div class="loading-fullpage">
-                    <div class="loading-circle">
-                        <div class="loading-circle1 loading-child"></div>
-                        <div class="loading-circle2 loading-child"></div>
-                        <div class="loading-circle3 loading-child"></div>
-                        <div class="loading-circle4 loading-child"></div>
-                        <div class="loading-circle5 loading-child"></div>
-                        <div class="loading-circle6 loading-child"></div>
-                        <div class="loading-circle7 loading-child"></div>
-                        <div class="loading-circle8 loading-child"></div>
-                        <div class="loading-circle9 loading-child"></div>
-                        <div class="loading-circle10 loading-child"></div>
-                        <div class="loading-circle11 loading-child"></div>
-                        <div class="loading-circle12 loading-child"></div>
-                    </div>
-                </div>
-EOT;
                 foreach ($data->toArray() as $row){
                     if(!empty($row)){
                         $dataArray1 = [

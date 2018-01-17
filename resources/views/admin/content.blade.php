84e9630747453730c7387ba441b6eaf9952296dc
WangYiNing <we820731@gmail.com>
2018-01-17 09:04:28 +0800

    新增 breadcrumb
    使用方法:
    $content->breadcrumb(
         ['text' => 'xxx', 'url' => '/xxx/xxx'],
         ['text' => 'yyy']
    );

--- resources/views/admin/content.blade.php
+++ resources/views/admin/content.blade.php
@@ -3,9 +3,36 @@
 @section('content')
     <section class="content-header">
         <h1>
-            {{ $header or trans('admin::lang.title') }}
-            <small>{{ $description or trans('admin::lang.description') }}</small>
+            {{ $header or trans('admin.title') }}
+            <small>{{ $description or trans('admin.description') }}</small>
         </h1>
+
+        <!-- breadcrumb start -->
+        {{--  @if ($breadcrumb)  --}}
+        <ol class="breadcrumb" style="margin-right: 30px;">
+            <li><a href="{{ admin_url('/') }}"><i class="fa fa-dashboard"></i> Home</a></li>
+            @foreach($breadcrumb as $item)
+                @if($loop->last)
+                    <li class="active">
+                        @if (array_has($item, 'icon'))
+                            <i class="fa fa-{{ $item['icon'] }}"></i>
+                        @endif
+                        {{ $item['text'] }}
+                    </li>
+                @else
+                <li>
+                    <a href="{{ admin_url(array_get($item, 'url')) }}">
+                        @if (array_has($item, 'icon'))
+                            <i class="fa fa-{{ $item['icon'] }}"></i>
+                        @endif
+                        {{ $item['text'] }}
+                    </a>
+                </li>
+                @endif
+            @endforeach
+        </ol>
+        {{--  @endif  --}}
+        <!-- breadcrumb end -->
 
     </section>
 

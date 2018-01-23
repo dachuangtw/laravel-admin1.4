4a73067099eacbf1494a1a81c9d3548f06362d53
淑貞 <shujhen@outlook.com>
2018-01-23 15:15:29 +0800

    資料庫微調

--- database/migrations/2018_01_18_132706_create_transfer_table.php
+++ database/migrations/2018_01_18_132706_create_transfer_table.php
@@ -15,11 +15,11 @@ class CreateTransferTable extends Migration
     {
         Schema::create('transfer', function (Blueprint $table) {
             $table->increments('tid')->comment('調撥單id');
-            $table->string('t_number',50)->comment('調撥單號');
+            $table->string('t_number',50)->index()->comment('調撥單號');
             $table->integer('wid_send')->unsigned()->index()->comment('出貨倉庫id');
             $table->integer('wid_receive')->unsigned()->index()->comment('進貨倉庫id');
             $table->integer('send_user')->unsigned()->index()->comment('調撥人id');
-            $table->integer('receive_user')->unsigned()->index()->comment('簽收人id');
+            $table->integer('receive_user')->unsigned()->index()->nullable()->comment('簽收人id');
             $table->decimal('t_amount',10,2)->default(0.00)->comment('總金額');
             $table->string('t_notes',100)->nullable()->comment('備註');
             $table->timestamp('send_at')->nullable()->comment('調撥時間');

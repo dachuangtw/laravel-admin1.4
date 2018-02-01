a0af5cc944ced2b7b0c14c1945a87bd1def5f6b2
WangYiNing <we820731@gmail.com>
2018-02-01 16:50:07 +0800

    業務領貨(未完成)

--- app/Admin/Controllers/SalesCollectController.php
+++ app/Admin/Controllers/SalesCollectController.php
@@ -3,7 +3,8 @@
 namespace App\Admin\Controllers;
 
 use App\SalesCollect;
-
+use App\Sales;
+use App\Warehouse;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Facades\Admin;
@@ -24,8 +25,11 @@ class SalesCollectController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header(trans('admin::lang.sales_collect'));
+            $content->description(trans('admin::lang.list'));
+            $content->breadcrumb(
+                ['text' => trans('admin::lang.sales_collect')]
+            );
 
             $content->body($this->grid());
         });
@@ -41,8 +45,12 @@ class SalesCollectController extends Controller
     {
         return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header(trans('admin::lang.sales_collect'));
+            $content->description(trans('admin::lang.edit'));
+            $content->breadcrumb(
+                ['text' => trans('admin::lang.sales_collect'), 'url' => '/sales/collect'],
+                ['text' => trans('admin::lang.edit')]
+            );
 
             $content->body($this->form()->edit($id));
         });
@@ -57,8 +65,12 @@ class SalesCollectController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header(trans('admin::lang.sales_collect'));
+            $content->description(trans('admin::lang.edit'));
+            $content->breadcrumb(
+                ['text' => trans('admin::lang.sales_collect'), 'url' => '/sales/collect'],
+                ['text' => trans('admin::lang.create')]
+            );
 
             $content->body($this->form());
         });
@@ -73,10 +85,44 @@ class SalesCollectController extends Controller
     {
         return Admin::grid(SalesCollect::class, function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
+            $grid->model()->orderBy('collect_date', 'desc'); // 預設排序
+            $grid->filter(function($filter){
+                $filter->disableIdFilter();
+                if(Admin::user()->isAdministrator()){
+                    $filter->where(function ($query) {
+                        $query->where('wid',  "{$this->input}");
+                    }, trans('admin::lang.warehouse'))->select(
+                        Warehouse::all()->pluck('w_name', 'wid')->toArray()
+                    );
+                }  
+                $filter->like('collect_id',trans('admin::lang.collect_id'));
+                $filter->between('collect_date', trans('admin::lang.collect_date'))->date();
+            });
+            $grid->number('No.')->sortable();
+            $grid->rows(function ($row, $number) {
+                $row->column('number', $number+1);
+            });
+            $grid->collect_date(trans('admin::lang.collect_date'))->sortable();
+            $grid->collect_id(trans('admin::lang.collect_id'))->sortable();
 
-            $grid->created_at();
-            $grid->updated_at();
+            //判斷是否為超級管理員，則只可看所屬倉庫內容
+            if(!Admin::user()->isAdministrator()){
+                $grid->model()->where('wid',Admin::user()->wid);    
+            }else{
+                $grid->wid(trans('admin::lang.warehouse'))->sortable()->display(function($wid) {
+                    return Warehouse::find($wid)->w_name;
+                })->label('info');
+            }
+            $grid->sales_id(trans('admin::lang.salesname'))->display(function($sales_id) {
+                    return Sales::find($sales_id)->name;
+                });
+            $grid->collect_amount(trans('admin::lang.collect_amount'))->sortable();
+            $grid->update_user(trans('admin::lang.update_user'))->display(function($userId) {
+                   return Administrator::find($userId)->name;
+            });
+
+            // $grid->created_at();
+            $grid->updated_at(trans('admin::lang.updated_at'));
         });
     }
 
@@ -89,10 +135,51 @@ class SalesCollectController extends Controller
     {
         return Admin::form(SalesCollect::class, function (Form $form) {
 
-            $form->display('id', 'ID');
+            // $form->display('id', 'ID');
+            if(strpos(url()->current(), '/edit') !== false) {
+                $form->date('collect_date',trans('admin::lang.collect_date'))->readOnly();
+                $form->text('collect_id',trans('admin::lang.collect_id'))->readOnly();
+                if(Admin::user()->isAdministrator()){
+                    $form->select('wid',trans('admin::lang.wid'))
+                        ->options(Warehouse::all()->pluck('w_name','wid'))->readOnly();
+                }else{
+                    $form->hidden('wid',trans('admin::lang.wid'))->value(Admin::user()->wid);
+                    $form->select('sales_id','業務'.trans('admin::lang.salesname'))
+                    ->options(Sales::all()->where('wid',Admin::user()->wid)->pluck('name','sales_id'))->readOnly(); 
+                }         
+            }else{
+                $form->date('collect_date',trans('admin::lang.collect_date'))->defaultdate('YYYY-MM-DD');
+                // $form->html('<font color="#333">系統自動產生</font>',trans('admin::lang.collect_id'));
+                //判斷超級使用者
+                if(Admin::user()->isAdministrator()){
+                    $form->select('wid',trans('admin::lang.wid'))
+                        ->options(Warehouse::all()->pluck('w_name','wid'));
+                    // $form->select('sales_id','業務'.trans('admin::lang.salesname'))
+                    // ->options(Sales::all()->where('wid',Admin::user()->wid)->pluck('name','sales_id'))
+                }else{
+                    $form->hidden('wid',trans('admin::lang.wid'))->value(Admin::user()->wid);
+                    
+                } 
+                $form->select('sales_id','業務'.trans('admin::lang.salesname'))
+                    ->options(Sales::all()->where('wid',Admin::user()->wid)->pluck('name','sales_id'));      
+            }
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->hidden('collect_id',trans('admin::lang.collect_id'));
+            $form->textarea('collect_notes',trans('admin::lang.notes'))->rows(2);
+            $form->hidden('update_user')->value(Admin::user()->id);
+            $form->hidden('collect_amount');
+            $form->divide();
+            $form->button('btn-danger btn-append','+ 配貨商品')->on('click','ShowModal("salescollect_hasstock");');
+
+            // $form->display('created_at', 'Created At');
+            // $form->display('updated_at', 'Updated At');
+
+             $form->saving(function (Form $form) {
+                
+
+
+
+             });
         });
     }
 }

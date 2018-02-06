d425e5e2912b1551c92db7324fc3ae95014eeae0
淑貞 <shujhen@outlook.com>
2018-02-06 12:16:06 +0800

    debug

--- database/migrations/2018_02_06_085518_create_inventory_table.php
+++ database/migrations/2018_02_06_085518_create_inventory_table.php
@@ -17,8 +17,8 @@ class CreateInventoryTable extends Migration
             $table->increments('inid');
             $table->string('in_number',50)->index()->comment('盤點單號');
             $table->integer('wid')->unsigned()->index()->comment('倉庫id');
-            $table->timestamp('start_at')->comment('盤點開始時間');
-            $table->timestamp('finish_at')->comment('盤點結束時間');
+            $table->timestamp('start_at')->nullable()->comment('盤點開始時間');
+            $table->timestamp('finish_at')->nullable()->comment('盤點結束時間');
             $table->integer('update_user')->comment('最後更新者id');
             $table->timestamps();
             $table->softDeletes();

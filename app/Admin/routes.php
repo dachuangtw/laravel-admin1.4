fb49dae1ca1f67c852e922db4b5ace086d447556
淑貞 <shujhen@outlook.com>
2018-02-09 10:13:48 +0800

    整理+註解

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,72 +10,71 @@ Route::group([
     'namespace'     => Admin::controllerNamespace(),
     'middleware'    => ['web', 'admin'],
 ], function (Router $router) {
-    // $attributes = ['middleware' => 'admin.permission:allow,administrator'];
-    //  $router->group($attributes, function ($router) {
-
-        // $router->resource('sales/endwork', SalesEndWorkController::class);
-        $router->resource('sales/log', SalesLogController::class);
-        $router->resource('sales/assign', SalesAssignController::class);
-        $router->resource('sales/collect', SalesCollectController::class);
-        $router->resource('sales/refund', SalesRefundController::class);
-        $router->resource('sales/notes', SalesNoteController::class);
-        $router->resource('sales', SalesController::class);
-
-        $router->resource('product/category', ProductCategoryController::class);
-        $router->resource('product/series', ProductSeriesController::class);
-        $router->resource('product/receipt', ProductReceiptController::class);
-        $router->resource('product', ProductIndexController::class);
-        $router->resource('supplier', ProductSupplierController::class);
-        $router->resource('web/location', WebLocationController::class);
-        $router->resource('web/area', WebAreaController::class);
-        $router->resource('warehouse', WarehouseController::class);
-        $router->resource('stock/category', StockCategoryController::class);
-
-        $router->resource('transfer', TransferController::class);
-        $router->resource('inventory', InventoryController::class);
-
-    // });
-
-    // $router->get('sales/endwork/{id}/view', 'SalesEndWorkController@view');
-    $router->get('sales/log/{id}/view', 'SalesLogController@view');
-    $router->get('sales/assign/{id}/view', 'SalesAssignController@view');
-    $router->get('sales/collect/{id}/view', 'SalesCollectController@view');
-    $router->get('sales/refund/{id}/view', 'SalesRefundController@view');
-    $router->get('sales/notes/{id}/view', 'SalesNoteController@view');
-    $router->get('sales/{id}/view', 'SalesController@view');
-
-    // $router->get('product/category/{id}/view', 'ProductCategoryController@view');
-    // $router->get('product/series/{id}/view', 'ProductSeriesController@view');
-    $router->get('supplier/{id}/view', 'ProductSupplierController@view');
-    $router->get('web/location/{id}/view', 'WebLocationController@view');
-    $router->get('web/area/{id}/view', 'WebAreaController@view');
-    $router->get('warehouse/{id}/view', 'WarehouseController@view');
-
+    /**
+     * 一般後台管理頁面，resource()自動生成router：
+     * POST      | store
+     * GET|HEAD  | index
+     * GET|HEAD  | create
+     * DELETE    | destroy
+     * GET|HEAD  | show
+     * PUT|PATCH | update
+     * GET|HEAD  | edit
+     */
+    $router->resource('sales/assign', SalesAssignController::class);
+    $router->resource('sales/collect', SalesCollectController::class);
+    $router->resource('sales/refund', SalesRefundController::class);
+    $router->resource('sales/notes', SalesNoteController::class);
+    $router->resource('sales', SalesController::class);
+    $router->resource('product/category', ProductCategoryController::class);
+    $router->resource('product/series', ProductSeriesController::class);
+    $router->resource('product/receipt', ProductReceiptController::class);
+    $router->resource('product', ProductIndexController::class);
+    $router->resource('supplier', ProductSupplierController::class);
+    $router->resource('web/area', WebAreaController::class);
+    $router->resource('web/location', WebLocationController::class);
+    $router->resource('warehouse', WarehouseController::class);
+    $router->resource('stock/category', StockCategoryController::class);
+    $router->resource('transfer', TransferController::class);
+    $router->resource('inventory', InventoryController::class);
+        
     /**
      * 眼睛查看
      */
+    //倉庫 (未完成)
+    $router->get('warehouse/{id}/view', 'WarehouseController@view');
+    //廠商 (未完成)
+    $router->get('supplier/{id}/view', 'ProductSupplierController@view');
     //商品
     $router->get('product/{id}/view', 'ProductIndexController@view');
     //調撥單
     $router->get('transfer/{id}/view', 'TransferController@view');
     //進貨單
     $router->get('product/receipt/{id}/view', 'ProductReceiptController@view');
-
-    $router->get('api/tw/district', 'WebLocationController@district');
+    //業務配貨
+    $router->get('sales/assign/{id}/view', 'SalesAssignController@view');
+    //業務領貨
+    $router->get('sales/collect/{id}/view', 'SalesCollectController@view');
+    //業務退貨 (未完成)
+    $router->get('sales/refund/{id}/view', 'SalesRefundController@view');
+    //店鋪據點 (未完成)
+    $router->get('web/location/{id}/view', 'WebLocationController@view');
     
-    //使用參數
+    /**
+     * 商品搜尋(彈出視窗)
+     */
+    //使用參數(方法1)
     // $router->get('modal', function () {
     //     $target = Input::get('t');
     //     return view('admin.modal',['target'=>$target]);
     // });
 
-    //使用路徑
+    //使用Controller(方法2)
+    // $router->get('modal/{target}', 'ProductIndexController@modal');
+
+    //使用路徑(方法3)
     $router->get('modal/{target}', function ($target) {
         return view('admin.modal',['target'=>$target]);
     });
-
-    //使用Controller
-    // $router->get('modal/{target}', 'ProductIndexController@modal');
 
     //商品搜尋的彈出/互動視窗modal
     $router->post('product/search', 'ProductIndexController@modalsearch');
@@ -85,24 +84,33 @@ Route::group([
     $router->post('product/receiptdetails', 'ProductIndexController@selectedproduct');
     $router->get('product/receiptdetails/{id}', 'ProductReceiptController@receiptdetails');
     
-    //盤點作業
-    $router->get('inventorydetails/getdata/{id}', 'InventoryDetailsController@getdata');    
-    $router->get('inventory/{id}/details', 'InventoryDetailsController@index');
-    $router->get('inventory/{id}/counting', 'InventoryController@counting');
-    $router->put('inventorydetails/{id}', 'InventoryDetailsController@update');
-    
     //調貨單
     $router->post('transfer/transferdetails', 'ProductIndexController@selectedproduct');
     $router->get('transfer/transferdetails/{id}', 'TransferController@transferdetails');
 
+    //業務配貨
     $router->post('sales/assigndetails', 'ProductIndexController@selectedproduct');
     $router->get('sales/assigndetails/{id}', 'SalesAssignController@salesassigndetails');
 
+    //業務領貨
     $router->post('sales/collectdetails', 'ProductIndexController@selectedproduct');
     $router->get('sales/collectdetails/{id}', 'SalesCollectController@salescollectdetails');
+    
+    /**
+     * 其他
+     */
+    //盤點作業
+    $router->get('inventorydetails/getdata/{id}', 'InventoryDetailsController@getdata');
+    $router->put('inventorydetails/{id}', 'InventoryDetailsController@update');
+    $router->get('inventory/{id}/details', 'InventoryDetailsController@index');
+    $router->post('inventory/{id}/search', 'InventoryDetailsController@search');
+
+    //鄉鎮市區選擇
+    $router->get('api/tw/district', 'WebLocationController@district');
 
     //商品匯入功能
     $router->post('product/import', 'ProductIndexController@import');
 
+    //後台管理首頁儀板表
     $router->get('/', 'HomeController@index');
 });

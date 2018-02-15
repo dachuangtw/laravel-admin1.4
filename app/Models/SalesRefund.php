faefa6d9f043441942eecd7bb2a9db3d2261456a
WangYiNing <we820731@gmail.com>
2018-02-15 10:08:14 +0800

    新增退貨model

--- app/Models/SalesRefund.php
+++ app/Models/SalesRefund.php
@@ -3,11 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SalesRefund extends Model
 {
+    //使用軟刪除
+    use SoftDeletes;
     //業務退貨資料表
     protected $table = 'sales_refund';
     //主鍵
     protected $primaryKey = 'srid';
+
+    protected $dates = ['deleted_at'];
+
+    //批量賦值
+    protected $fillable = [
+        'update_user',  'updated_at', 'deleted_at'
+    ];
+    
 }
--- database/migrations/2018_02_09_101921_create_sales_refund_details_table.php
+++ database/migrations/2018_02_09_101921_create_sales_refund_details_table.php
@@ -20,6 +20,7 @@ class CreateSalesRefundDetailsTable extends Migration
             $table->integer('stid')->unsigned()->index()->comment('庫存id');
             $table->decimal('srd_salesprice',10,2)->default(0.00)->comment('單價(業務價)');
             $table->integer('srd_quantity')->default(0)->comment('數量');
+            $table->integer('srd_discount')->default(100.00)->comment('退貨折數');
             $table->decimal('srd_amount')->default(0.00)->comment('總計金額');    
             $table->boolean('srd_check')->nullable()->comment('點貨確認');      
             $table->text('srd_notes')->nullable()->comment('商品備註');

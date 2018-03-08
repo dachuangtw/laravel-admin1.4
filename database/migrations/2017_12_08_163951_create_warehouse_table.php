ec994cb822e07ac8996f999967bbdcdf9f0c73dd
淑貞 <shujhen@outlook.com>
2018-03-08 10:27:09 +0800

    修改倉庫資料庫遷移檔

--- database/migrations/2017_12_08_163951_create_warehouse_table.php
+++ database/migrations/2017_12_08_163951_create_warehouse_table.php
@@ -13,14 +13,14 @@ class CreateWarehouseTable extends Migration
      */
     public function up()
     {
-        Schema::create('warehouse', function (Blueprint $table) {
-            $table->increments('wid')->comment('倉庫id');
-            $table->string('w_name',25)->comment('倉庫名');
+        Schema::connection('mysql2')->create('warehouses', function (Blueprint $table) {
+            $table->increments('id')->comment('倉庫id');
+            $table->string('name',25)->comment('倉庫名');
             $table->integer('parent_id')->default(0)->comment('父級分類id');
-            $table->integer('w_sort')->default(0)->comment('順序');
-            $table->string('w_phone',50)->nullable()->comment('連絡電話');
-            $table->string('w_address',200)->nullable()->comment('地址');
-            $table->text('w_notes')->nullable()->comment('備註');
+            $table->integer('sort')->default(0)->comment('順序');
+            $table->string('phone',50)->nullable()->comment('連絡電話');
+            $table->string('address',200)->nullable()->comment('地址');
+            $table->text('notes')->nullable()->comment('備註');
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateWarehouseTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('warehouse');
+        Schema::connection('mysql2')->dropIfExists('warehouses');
     }
 }

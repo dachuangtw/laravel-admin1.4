6c89551be471622a61c50e837003f4b9dc9232dc
WangYiNing <we820731@gmail.com>
2018-03-13 13:20:52 +0800

    更新領貨單
    刪除領貨單商品數量退回庫存

--- app/Models/SalesCollect.php
+++ app/Models/SalesCollect.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App;
-
+use Encore\Admin\Facades\Admin;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -27,12 +27,34 @@ class SalesCollect extends Model
        return $this->hasMany(SalesCollectDetails::class,'collect_id','collect_id');
     }
     
-    // 刪除領貨單同時刪除領貨明細
+    // 刪除領貨單同時刪除領貨明細 新增log紀錄
     protected static function boot()
     {
         parent::boot();
-        static::deleting(function($details) {
+        static::deleted(function($model) {
             // $details->SalesCollectDetails()->delete();
+            $SalesCollectDetails = SalesCollectDetails::where('collect_id',$model->collect_id)->pluck('scd_quantity','stid');
+
+            $insertStockLogArray = [];
+            foreach($SalesCollectDetails as $stid => $quantity){
+                $stock = Stock::where('stid',$stid)->select('pid', 'wid','st_stock')->first();
+
+                $st_stock = (int) $stock->st_stock + (int) $quantity;
+                Stock::find($stid)->update(['st_stock' => $st_stock]);
+
+                $insertStockLogArray[] = [
+                    'pid'          =>  $stock->pid,
+                    'wid'          =>  $stock->wid,
+                    'stid'         =>  $stid,
+                    'sl_calc'      =>  '+',
+                    'sl_quantity'  =>  $quantity,
+                    'sl_stock'     =>  $st_stock,
+                    'sl_notes'     =>  '領貨單：'.$model->collect_id.'-刪除',
+                    'update_user'  =>  Admin::user()->id,
+                    'updated_at'    =>  date('Y-m-d H:i:s'),
+                ];
+            }
+            $insertStockLogArray && StockLog::insert($insertStockLogArray);
         });
     }
 }

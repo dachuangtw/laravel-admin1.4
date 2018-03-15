f88d825be3635e42619c272ecec86b14bbb8cca8
WangYiNing <we820731@gmail.com>
2018-03-15 16:54:20 +0800

    更新儀錶板內容

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use App\ProductIndex;
 use App\WebLocation;
 use App\ProductCategory;
 use App\Warehouse;
-
+use App\SalesCollect;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Column;
@@ -37,7 +37,7 @@ class HomeController extends Controller
 
             $content->row(function ($row) {
                 $row->column(2, new InfoBox('業務人數', 'users', 'aqua', '/admin/sales', Sales::where(['resign' => 0])->count()));
-                $row->column(2, new InfoBox('業務訂單', 'shopping-cart', 'green', '/admin/orders', '150%'));
+                $row->column(2, new InfoBox('業務訂單', 'shopping-cart', 'green', '/admin/sales/collect', SalesCollect::where(['collect_date' => date('Y-m-d')])->count()));
                 $row->column(2, new InfoBox('業務公告', 'file', 'red', '/admin/sales/notes', SalesNotes::count()));
                 $row->column(2, new InfoBox('今日領貨量', 'check-square', 'purple', '/admin', '0'));
                 $row->column(2, new InfoBox('商品樣數', 'cubes', 'orange', '/admin/product', ProductIndex::count()));
@@ -69,78 +69,78 @@ class HomeController extends Controller
                 });
 
             });
-            $content->row(function (Row $row) {
+    //         $content->row(function (Row $row) {
 
 
-                $row->column(6, function (Column $column) {
+    //             $row->column(6, function (Column $column) {
 
-                    $tab = new Tab();
+    //                 $tab = new Tab();
 
 
 
-                    $tab->add('Table', new Table());
-                    $tab->add('Text', 'blablablabla....');
+    //                 $tab->add('Table', new Table());
+    //                 $tab->add('Text', 'blablablabla....');
 
-                    $tab->dropDown([['Orders', '/admin/orders'], ['administrators', '/admin/administrators']]);
-                    $tab->title('Tab');
+    //                 $tab->dropDown([['Orders', '/admin/orders'], ['administrators', '/admin/administrators']]);
+    //                 $tab->title('Tab');
 
-                    $column->append($tab);
+    //                 $column->append($tab);
 
-                    $collapse = new Collapse();
+    //                 $collapse = new Collapse();
 
-                    $bar = new Bar(
-                        ["January", "February", "March", "April", "May", "June", "July"],
-                        [
-                            ['First', [40,56,67,23,10,45,78]],
-                            ['Second', [93,23,12,23,75,21,88]],
-                            ['Third', [33,82,34,56,87,12,56]],
-                            ['Forth', [34,25,67,12,48,91,16]],
-                        ]
-                    );
-                    $collapse->add('Bar', $bar);
-                    $collapse->add('Orders', new Table());
-                    $column->append($collapse);
+    //                 $bar = new Bar(
+    //                     ["January", "February", "March", "April", "May", "June", "July"],
+    //                     [
+    //                         ['First', [40,56,67,23,10,45,78]],
+    //                         ['Second', [93,23,12,23,75,21,88]],
+    //                         ['Third', [33,82,34,56,87,12,56]],
+    //                         ['Forth', [34,25,67,12,48,91,16]],
+    //                     ]
+    //                 );
+    //                 $collapse->add('Bar', $bar);
+    //                 $collapse->add('Orders', new Table());
+    //                 $column->append($collapse);
 
-                    $doughnut = new Doughnut([
-                        ['Chrome', 700],
-                        ['IE', 500],
-                        ['FireFox', 400],
-                        ['Safari', 600],
-                        ['Opera', 300],
-                        ['Navigator', 100],
-                    ]);
-                    $column->append((new Box('Doughnut', $doughnut))->removable()->collapsable()->style('info'));
-                });
+    //                 $doughnut = new Doughnut([
+    //                     ['Chrome', 700],
+    //                     ['IE', 500],
+    //                     ['FireFox', 400],
+    //                     ['Safari', 600],
+    //                     ['Opera', 300],
+    //                     ['Navigator', 100],
+    //                 ]);
+    //                 $column->append((new Box('Doughnut', $doughnut))->removable()->collapsable()->style('info'));
+    //             });
 
-                $row->column(6, function (Column $column) {
+    //             $row->column(6, function (Column $column) {
 
-                    $column->append(new Box('Radar', new Radar()));
+    //                 $column->append(new Box('Radar', new Radar()));
 
-                    $polarArea = new PolarArea([
-                        ['Red', 300],
-                        ['Blue', 450],
-                        ['Green', 700],
-                        ['Yellow', 280],
-                        ['Black', 425],
-                        ['Gray', 1000],
-                    ]);
-                    $column->append((new Box('Polar Area', $polarArea))->removable()->collapsable());
+    //                 $polarArea = new PolarArea([
+    //                     ['Red', 300],
+    //                     ['Blue', 450],
+    //                     ['Green', 700],
+    //                     ['Yellow', 280],
+    //                     ['Black', 425],
+    //                     ['Gray', 1000],
+    //                 ]);
+    //                 $column->append((new Box('Polar Area', $polarArea))->removable()->collapsable());
 
-                    $column->append((new Box('Line', new Line()))->removable()->collapsable()->style('danger'));
-                });
+    //                 $column->append((new Box('Line', new Line()))->removable()->collapsable()->style('danger'));
+    //             });
 
-            });
+    //         });
 
-            $headers = ['Id', 'Email', 'Name', 'Company', 'Last Login', 'Status'];
-            $rows = [
-                [1, 'labore21@yahoo.com', 'Ms. Clotilde Gibson', 'Goodwin-Watsica', '1997-08-13 13:59:21', 'open'],
-                [2, 'omnis.in@hotmail.com', 'Allie Kuhic', 'Murphy, Koepp and Morar', '1988-07-19 03:19:08', 'blocked'],
-                [3, 'quia65@hotmail.com', 'Prof. Drew Heller', 'Kihn LLC', '1978-06-19 11:12:57', 'blocked'],
-                [4, 'xet@yahoo.com', 'William Koss', 'Becker-Raynor', '1988-09-07 23:57:45', 'open'],
-                [5, 'ipsa.aut@gmail.com', 'Ms. Antonietta Kozey Jr.', 'Braun Ltd', '2013-10-16 10:00:01', 'open'],
-            ];
+    //         $headers = ['Id', 'Email', 'Name', 'Company', 'Last Login', 'Status'];
+    //         $rows = [
+    //             [1, 'labore21@yahoo.com', 'Ms. Clotilde Gibson', 'Goodwin-Watsica', '1997-08-13 13:59:21', 'open'],
+    //             [2, 'omnis.in@hotmail.com', 'Allie Kuhic', 'Murphy, Koepp and Morar', '1988-07-19 03:19:08', 'blocked'],
+    //             [3, 'quia65@hotmail.com', 'Prof. Drew Heller', 'Kihn LLC', '1978-06-19 11:12:57', 'blocked'],
+    //             [4, 'xet@yahoo.com', 'William Koss', 'Becker-Raynor', '1988-09-07 23:57:45', 'open'],
+    //             [5, 'ipsa.aut@gmail.com', 'Ms. Antonietta Kozey Jr.', 'Braun Ltd', '2013-10-16 10:00:01', 'open'],
+    //         ];
 
-            $content->row((new Box('Table', new Table($headers, $rows)))->style('info')->solid());
+    //         $content->row((new Box('Table', new Table($headers, $rows)))->style('info')->solid());
         });
     }
 }
